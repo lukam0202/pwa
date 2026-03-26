a4f9b0fb94c4b2eadc92fb93c0080cae45f31241
Luka M <66641620+lukam0202@users.noreply.github.com>
2026-03-26 16:11:59 +0100

    Vjezba 1 za PWA

---
+++ vjezba1.php
@@ -0,0 +1,15 @@
+<?php
+$naslov = "Moj prvi zadatak i prvi PHP dokument";
+$autor = "Luka Mikić";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je <strong>$autor</strong></p>";
+
+echo '<a href="https://www.youtube.com"
+target="_blank">Posjeti youtube.com</a>';
+
+?>
+
+<!DOCTYPE html>
+
+<!-- Naziv datoteke: vjezba1.php -->
